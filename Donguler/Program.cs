fe4f316e806cb5d817a4be094b808f0be36320c4
halil temel <haliltml91@gmail.com>
2021-01-17 01:55:39 +0300

    16 ocak ders sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] {"Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya Başlangıç için Temel Kurs", "Java", "Python", "C#" };
+
+            
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+
+            foreach (string kurs in kurslar) //foreach genellikle dizileri daha kolay dolaşmak için kullanılır
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu");
+        }
+    }
+}
